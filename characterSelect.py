3221fbf62ff534d053ad6a87538106911109f18c
jkato23 <163766524+jkato23@users.noreply.github.com>
2024-05-14 12:50:45 -0700

    Add files via upload

---
+++ characterSelect.py
@@ -0,0 +1,68 @@
+characters = ("Yun", "Gouki", "Remy", "Ryu", "Urien", "Q", "Oro", "Necro", "Chun-Li", "Dudley", "Ibuki", "Makoto", "Elena", "Sean", "Twelve", "Hugo", "Alex", "Yang", "Ken", "Gill")
+supers = {
+    "Yun": ("", "I Youhou", "II Sourai Rengeki", "III Genei Jin"),
+    "Gouki": ("", "I Messatsu Gou Hadou", "II Messatsu Gou Shouryuu", "III Messatsu Gou Rasen"),
+    "Remy": ("", "I Hunnu no Supernova", "II Vierge ni Ansoku O", "III Shoushin no Nocturne"),
+    "Ryu": ("", "I Shinkuu Hadou Ken", "II Shin Shouryuu Ken", "III Denjin Hadou Ken"),
+    "Urien": ("", "I Tyrant Punish", "II Jupiter Thunder", "III Aegis Reflector"),
+    "Q": ("", "I Tosshin Oyobi Chishi Renzoku Dageki (Kari)", "II Fukubu Oyobi Koutoubu e no Tsuuda (Kari)", "III Bakuhatsu o Tomonau Dageki ya Hokaku (Kari)"),
+    "Oro": ("", "I Kishin Riki", "II Yagyou Dama", "III Tengu Ishi"),
+    "Necro": ("", "I Chou Denji Storm", "II Slam Dance", "III Electric Snake"),
+    "Chun-Li": ("", "I Kikou Shou", "II Houyoku Sen", "III Tensei Ranka"),
+    "Dudley": ("", "I Rocket Upper", "II Rolling Thunder", "III Corkscrew Blow"),
+    "Ibuki": ("", "I Kasumi Suzaku", "II Yoroi Dooshi", "III Yami Shigure"),
+    "Makoto": ("", "I Seichuusen Godan-zuki", "II Abare Tosanami Kudaki", "III Tanden Renki: Seme no Kata"),
+    "Elena": ("", "I Spinning Beat", "II Brave Dance", "III Healing"),
+    "Sean": ("", "I Hadou Burst", "II Shouryuu Cannon", "III Hyper Tornado"),
+    "Twelve": ("", "I X.N.D.L", "II X.F.L.A.T", "III X.C.O.P.Y"),
+    "Hugo": ("", "I Gigas Breaker", "II Megaton Press", "III Hammer Mountain"),
+    "Alex": ("", "I Hyper Bomb", "II Boomerang Raid", "III Stun Gun Headbutt"),
+    "Yang": ("", "I Raishin Mahha Ken", "II Tenshin Senkyuutai", "III Sei'ei Enbu"),
+    "Ken": ("", "I Shouryuu Reppa", "II Shinryuu Ken", "III Shippuujinrai Kyaku"),
+    "Gill": ("", "I Meteor Strike", "II Seraphic Wing", "III Ressurection")
+}
+
+def chooseCharacter():
+    global charSelect
+    selectScreen = " ".join(str(i) for i in characters)
+    print(selectScreen + "\n")
+    charSelect = input("Which character do you choose? ")
+    charSelect = charSelect.capitalize()
+    if (charSelect in characters):
+        if (charSelect == "Gill"):
+            print("\n" "Gill is not a selectable character. Please choose another character." "\n")
+            chooseCharacter()
+        else:
+            print("\n" "Your character is " + charSelect + "." "\n")
+    else:
+        print("\n" "That is not a valid character selection. Please choose again." "\n")
+        chooseCharacter()
+
+def chooseSuper(charSelect):
+    for value in supers[charSelect][1:4]:
+        print(value)
+    try:
+        superSelect = int(input("\n" "Which super art do you choose? Enter a number between 1 and 3: "))
+        if (superSelect > 3 or superSelect < 1):
+            print("\n" "That is not a valid super art selection. Please choose again." "\n")
+            chooseSuper(charSelect)
+        else:
+            print("\n" + "Your character is " + charSelect + " and your super art is " + (supers[charSelect][superSelect] + "."))
+            if (charSelect == "Gouki"):
+                if (superSelect == 1):
+                    print("You also have access to the super arts Tenma Gou Zankuu, Shun Goku Satsu, and Kongou Kokuretsu Zan.")
+                elif (superSelect == 3):
+                    print("You also have access to the super arts Messatsu Gou Senpuu, Shun Goku Satsu, and Kongou Kokuretsu Zan.")
+            if (charSelect == "Oro"):
+                if (superSelect == 1):
+                    print("You also have access to the super arts Kishin Kuuchuu Jigoku Guruma and Kishin Tsui.")
+                if (superSelect == 2):
+                    print("You also have access to the super art Yagyou Oodama.")
+                if (superSelect == 3):
+                    print("You also have access to the suepr art Tengu Midare Ishi.")
+    except ValueError:
+        print("\n" "You entered something that was not a number. Please choose again." "\n")
+        chooseSuper(charSelect)
+
+chooseCharacter()
+chooseSuper(charSelect)
